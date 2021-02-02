4800882cd1cf0defc1dfe39db46cee368b8ad8c6
J.Lee-SAIT Laptop <jinseok.lee@edu.sait.ca>
2021-02-01 22:43:28 -0700

    Link to Login page

--- app.js
+++ app.js
@@ -5,18 +5,30 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const path = require("path");
+const bodyParser = require('body-parser');
 require("dotenv/config");
 
+// HTML default setting
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+// application uses
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
 //exported route from other.js
-const otherRoute = require("./routes/other");
+const otherRoute = require("./routes/otherRoutes");
+const loginRoute = require("./routes/loginRoutes");
 app.use("/other", otherRoute);
+app.use("/login", loginRoute);
 
 //start listening & setup route
 app.listen(3000);
 
 //localhost:3000
 app.get("/", (req, res) => {
-  res.send("This is the login page");
+  res.send("This is the main page <br/> <a href='/login'>Move to Login Page</a>");
 });
 
 //app.post("/", (req, res) => {});
