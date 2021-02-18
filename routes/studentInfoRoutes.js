26725d6a1fa68f9873e5bde898ccb08d095174a5
J.Lee-SAIT Laptop <jinseok.lee@edu.sait.ca>
2021-02-18 01:28:13 -0700

    Student Registration function implemented: 80%

--- routes/studentInfoRoutes.js
+++ routes/studentInfoRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const router = express.Router();
 const bodyParser = require("body-parser");
-const User = require("../models/userSchema");
+const Student = require("../models/studentSchema");
 const bcrypt = require("bcrypt");
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -14,11 +14,53 @@ router.get("/", (req, res, next) => {
 
 router.get("/register", (req, res, next) => {
     var payload = createPayload(req.session.user);
-    res.status(200).render("users/studentRegistration", payload);
+    res.status(200).render("users/studentRegister", payload);
 });
 
-router.post("/", async (req, res, next) => {
-    res.send("Hello this post is working");
+router.post("/register", async (req, res, next) => {
+    var saitId = req.body.saitId.trim();
+    var firstName = req.body.firstName.trim();
+    var lastName = req.body.lastName.trim();
+    var studentPhone = req.body.studentPhone.trim();
+    var studentEmail = req.body.studentEmail.trim();
+    var personalEmail = req.body.personalEmail.trim();
+    var academicStatus = req.body.academicStatus.trim();
+    var comments = req.body.comments.trim();
+
+    var payload = req.body;
+
+    if(saitId && firstName && lastName) {
+        var student = await Student.findOne({ saitId: saitId })
+        .catch(err => {
+            console.log(err);
+            payload.result = "fail"
+            payload.message = "Something went wrong.";
+            res.status(200).render("users/studentRegister", payload);
+        });
+
+        if (student == null) {
+            //No student found
+            var data = req.body;
+
+            Student.create(data)
+            .then(student => {
+                payload = {
+                    result: "success",
+                    message: `New student #${saitId} successfully added.`
+                }
+                res.status(200).render("users/studentRegister", payload);
+            });
+        }
+        else {
+            payload.result = "fail";
+            payload.message = `Provided SAIT ID #${saitId} is already registered.`;
+            res.status(200).render("users/studentRegister", payload);
+        }
+    }
+    else {
+        payload.result = "fail";
+        payload.message = "Make sure each field has a valid value.";
+    }
 });
 
 function createPayload(userLoggedIn) {
