ee8dadb86c86d0d3ab07e33a42163e6a6238712c
Anzerous <78055480+Anzerous@users.noreply.github.com>
2021-04-12 19:51:15 -0600

    workshop update complete

--- routes/workshopRoutes.js
+++ routes/workshopRoutes.js
@@ -20,14 +20,16 @@ router.get("/workshopNew", (req, res, next) => {
 
 router.post("/workshopNew", async (req, res, next) => {
   var workshop = {
+    workshopRequest: req.body.workshopRequest,
     workshopID: req.body.workshopID.trim(),
     workshopType: req.body.workshopType.trim(),
     workshopDesc: req.body.workshopDesc.trim(),
     workshopDate: req.body.workshopDate.trim(),
     workshopLength: req.body.workshopLength.trim(),
     workshopFacilitator: req.body.workshopFacilitator.trim(),
-    workshopRequest: req.body.workshopRequest,
     comments: req.body.comments.trim(),
+    numAttendees: req.body.numAttendees.trim(),
+    avgRating: req.body.avgRating.trim(),
   }
 
   if (workshop.workshopRequest == "on"){
@@ -35,19 +37,15 @@ router.post("/workshopNew", async (req, res, next) => {
     workshop.requestProgram = req.body.requestProgram.trim();
     workshop.requestSchool = req.body.requestProgram.trim();
     workshop.requestContact = req.body.requestContact.trim();
-    workshop.numAttendees = req.body.numAttendees.trim();
-    workshop.avgRating = req.body.avgRating.trim();
   } else {
     workshop.workshopRequest = false;
     workshop.requestProgram = "";
     workshop.requestSchool = "";
     workshop.requestContact = "";
-    workshop.numAttendees = "";
-    workshop.avgRating = "";
   }
 
   if (workshop) {
-    var workshop = await Workshop.findOne({ workshopID: workshop.workshopID })
+    var workshopFound = await Workshop.findOne({ workshopID: workshop.workshopID })
       .catch(err => {
         console.log(err);
         payload.success = false;
@@ -55,7 +53,7 @@ router.post("/workshopNew", async (req, res, next) => {
         res.status(200).render("users/workshopNew", payload);
       });
 
-    if (workshop == null) {
+    if (workshopFound == null) {
       Workshop.create(workshop)
         .then(workshop => {
           payload = {
@@ -81,11 +79,34 @@ router.get("/info/:workshopID", async (req, res, next) => {
 
 router.post("/info/:workshopID", async (req, res, next) => {
   //textarea needs to be trim
-  req.body.comments = req.body.comments.trim();
+  var workshop = {
+    workshopRequest: req.body.workshopRequest,
+    workshopID: req.params.workshopID.trim(),
+    workshopType: req.body.workshopType.trim(),
+    workshopDesc: req.body.workshopDesc.trim(),
+    workshopDate: req.body.workshopDate.trim(),
+    workshopLength: req.body.workshopLength.trim(),
+    workshopFacilitator: req.body.workshopFacilitator.trim(),
+    comments: req.body.comments.trim(),
+    numAttendees: req.body.numAttendees.trim(),
+    avgRating: req.body.avgRating.trim(),
+  }
+
+  if (workshop.workshopRequest == "on"){
+    workshop.workshopRequest = true;
+    workshop.requestProgram = req.body.requestProgram.trim();
+    workshop.requestSchool = req.body.requestProgram.trim();
+    workshop.requestContact = req.body.requestContact.trim();
+  } else {
+    workshop.workshopRequest = false;
+    workshop.requestProgram = "";
+    workshop.requestSchool = "";
+    workshop.requestContact = "";
+  }
 
   var payload = await getPayload(req.params.workshopID);
 
-  var workshop = await Workshop.findOneAndUpdate({workshopID: req.params.workshopID}, {$set: req.body}, {new: true})
+  var workshopUpdated = await Workshop.findOneAndUpdate({workshopID: req.params.workshopID}, {$set: workshop}, {new: true})
   .catch(error => {
       console.log(error);
       res.sendStatus(400);
@@ -95,7 +116,7 @@ router.post("/info/:workshopID", async (req, res, next) => {
   payload.workshopID = req.params.workshopID;
   payload.success = true;
   payload.message = `Workshop ${req.params.workshopID} successfully updated.`;
-  payload.appointmentInfo = workshop;
+  payload.workshopInfo = workshopUpdated;
   res.status(200).render("users/workshopInfo", payload);
 });
 
