add4698ba05cfac436831f69414dcb966a1cbbf0
mitchellouch <67443440+mitchellouch@users.noreply.github.com>
2021-04-15 09:30:28 -0600

    deploy

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ app.use("/api/workshops", middleware.requireLogin, workshopsApiRoute);
 app.use("/api/admin", middleware.requireAdmin, adminApiRoute);
 
 //start listening & setup route
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8081;
 app.listen(port, () => {
   console.log("Server listening on port " + port);
 });
